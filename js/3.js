9f2a361ea033821b3c509ebbd59c577c7ac29fbf
Lisandro Civili <lisandrocivili13@gmail.com>
2023-11-11 17:40:09 -0300

    Ejercicio 3 incompleto, pospuesto

---
+++ js/3.js
@@ -0,0 +1,18 @@
+const resultados = [];
+
+for (let i = 0; i < 50; i++) {
+    const dado1 = Math.floor(Math.random() * 6) + 1;
+    const dado2 = Math.floor(Math.random() * 6) + 1;
+    
+    const sumaDados = dado1 + dado2;
+
+    resultados[i] = sumaDados;
+
+    document.write(`Resultado ${i+1}: ${resultados[i]} <br>`)
+}
+
+for (let i = 0; i < array.length; i++) {
+
+    
+
+}
